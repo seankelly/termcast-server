13307372b9d96a0d87d1ad9e1a97e282f090e4b4
Sean Kelly <code@katron.org>
2015-05-05 22:27:11 -0500

    Crudely adapt mio example
    Can listen on a port, and that's it.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
 extern crate mio;
 
+use mio::*;
+use mio::tcp::TcpListener;
+
+
+const SERVER: Token = Token(0);
+const CLIENT: Token = Token(1);
+
+
+struct Termcastd (NonBlock<TcpListener>);
+
+
+impl Handler for Termcastd {
+    type Timeout = ();
+    type Message = ();
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let addr = "127.0.0.1:31337".parse().unwrap();
+    let server = tcp::listen(&addr).unwrap();
+
+    let mut event_loop = EventLoop::new().unwrap();
+    event_loop.register(&server, SERVER).unwrap();
+
+    event_loop.run(&mut Termcastd(server)).unwrap();
 }
