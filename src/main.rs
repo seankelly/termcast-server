56debae902f91efb209363ab887519b80ae47531
Sean Kelly <code@katron.org>
2015-05-07 21:41:26 -0500

    Accept watcher socket and add it to the vector

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,48 @@ extern crate mio;
 
 use mio::*;
 use mio::tcp::TcpListener;
+use mio::tcp::TcpStream;
 
 
 const CASTER: Token = Token(0);
 const WATCHER: Token = Token(1);
 
 
+struct Watcher {
+    sock: NonBlock<TcpStream>,
+}
+
 struct Termcastd {
     listen_caster: NonBlock<TcpListener>,
     listen_watcher: NonBlock<TcpListener>,
     casters: Vec<NonBlock<TcpStream>>,
+    watchers: Vec<Watcher>,
 }
 
 
 impl Handler for Termcastd {
     type Timeout = ();
     type Message = ();
+
+    fn readable(&mut self, event_loop: &mut EventLoop<Termcastd>, token: Token, hint: ReadHint) {
+        match token {
+            CASTER => {
+            },
+            WATCHER => {
+                if let Ok(opt) = self.listen_watcher.accept() {
+                    if let Some(sock) = opt {
+                        let watcher = Watcher {
+                            sock: sock,
+                        };
+                        self.watchers.push(watcher);
+                    }
+                }
+                else {
+                }
+            },
+            _ => {},
+        }
+    }
 }
 
 
@@ -36,6 +62,7 @@ fn main() {
         listen_caster: listen_caster,
         listen_watcher: listen_watcher,
         casters: Vec::new(),
+        watchers: Vec::new(),
     };
     event_loop.run(&mut termcastd).unwrap();
 }
