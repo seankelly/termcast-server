4e59e18d9bf889904b510e395d8b39048ed1cb77
Sean Kelly <code@katron.org>
2015-10-15 21:47:31 -0500

    Comment what each byte means in disabling echoing

--- src/term.rs
+++ src/term.rs
@@ -1,4 +1,7 @@
 
 pub fn clear_screen() -> &'static str { "\x1b[2J" }
 pub fn reset_cursor() -> &'static str { "\x1b[H" }
-pub fn disable_local_echo() -> [u8; 3] { [0xff, 0xfb, 0x01] }
+pub fn disable_local_echo() -> [u8; 3] {
+    /* IAC WILL  ECHO */
+    [0xff, 0xfb, 0x01]
+}
