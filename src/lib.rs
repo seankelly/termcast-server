88f49346c35222d6127bdeb4bb864afdc7f58ffd
Sean Kelly <code@katron.org>
2016-01-01 20:58:43 -0600

    Include how many watchers there are
    Also include spots for how long the caster has been idle and how long
    the caster has been connected.

--- src/lib.rs
+++ src/lib.rs
@@ -306,7 +306,8 @@ impl Termcastd {
 
     fn watcher_menu(&self, offset: usize) -> Vec<u8> {
         fn caster_menu_entry(choice: &'static str, caster: &Caster, unknown_name: &String) -> String {
-            format!(" {}) {}\r\n", choice, caster.name.as_ref().unwrap_or(unknown_name))
+            format!(" {}) {} (idle ???, connected ???, {} watching)\r\n",
+                    choice, caster.name.as_ref().unwrap_or(unknown_name), caster.watchers.len())
         }
 
         let mut offset = offset;
