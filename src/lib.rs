a872b05fae9e5f9d0be6d75b32ff8937f707fa88
Sean Kelly <code@katron.org>
2016-01-04 14:53:25 -0600

    Remove unused variable

--- src/lib.rs
+++ src/lib.rs
@@ -545,7 +545,6 @@ impl Termcastd {
 
                 self.number_watching += 1;
 
-                let offset = watcher.offset;
                 self.clients.insert(token, Client::Watcher);
                 self.watchers.insert(token, watcher);
 
