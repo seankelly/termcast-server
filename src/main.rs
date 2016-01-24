add0a12e357d151afb0e91fa20aeff6aff61f866
Sean Kelly <code@katron.org>
2016-01-24 15:04:49 -0600

    Use default function

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,7 @@ use termcastd::TermcastServer;
 use termcastd::config::TermcastConfig;
 
 fn main() {
-    let tc_config = TermcastConfig {
-        caster: "127.0.0.1:31337".parse().unwrap(),
-        watcher: "127.0.0.1:2300".parse().unwrap(),
-        motd: None,
-    };
+    let tc_config = TermcastConfig::default();
 
     if let Ok(mut termcast) = TermcastServer::new(tc_config) {
         termcast.run();
