9f0a40b52069d93ad14707ad3b090c8b5941db84
Sean Kelly <code@katron.org>
2016-01-24 22:39:35 -0600

    Start trying to parse options
    Take a -f/--foreground option to avoid forking. Right now, this is the
    default behavior. Also support -c/--config for specifying a different
    config. This doesn't quite yet do anything.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
 extern crate getopts;
 extern crate termcastd;
 
+use getopts::Options;
+use std::env;
 use termcastd::TermcastServer;
 use termcastd::config::TermcastConfig;
 
+fn get_options() -> TermcastConfig {
+    let args: Vec<String> = env::args().collect();
+    let mut options = Options::new();
+    options.optflag("f", "foreground", "Run in the foreground.");
+    options.optopt("c", "config", "Configuration file to use.", "FILE");
+
+    let matches = match options.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(e) => panic!(e.to_string()),
+    };
+
+    let tc_config = match matches.opt_str("c") {
+        Some(_config_file) => { TermcastConfig::default() },
+        None => TermcastConfig::default(),
+    };
+
+    return tc_config;
+}
+
 fn main() {
-    let tc_config = TermcastConfig::default();
+    let tc_config = get_options();
 
     if let Ok(mut termcast) = TermcastServer::new(tc_config) {
         termcast.run();
