0eb2f533e99f0057a11835c185c56c2677d5e116
Sean Kelly <code@katron.org>
2016-05-06 22:27:58 -0500

    Add constants for the config
    This needs to be extended to support a config file.

--- src/config.rs
+++ src/config.rs
@@ -7,11 +7,15 @@ pub struct TermcastConfig {
     pub motd: Option<String>,
 }
 
+const CASTER_LISTEN: &'static str = "127.0.0.1:31337";
+const WATCHER_LISTEN: &'static str = "127.0.0.1:2300";
+const MOTD: Option<String> = None;
+
 impl Default for TermcastConfig {
     fn default() -> Self {
         TermcastConfig {
-            caster: "127.0.0.1:31337".parse().unwrap(),
-            watcher: "127.0.0.1:2300".parse().unwrap(),
+            caster: CASTER_LISTEN.parse().unwrap(),
+            watcher: WATCHER_LISTEN.parse().unwrap(),
             motd: None,
         }
     }
