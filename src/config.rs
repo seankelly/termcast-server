87be321e084773001e71103370074cac4c548a70
Sean Kelly <code@katron.org>
2016-05-18 22:28:47 -0500

    Handle errors with the caster and watcher listen addresses

--- src/config.rs
+++ src/config.rs
@@ -2,15 +2,22 @@ use std::default::Default;
 use std::fs::File;
 use std::io::Read;
 use std::io::Error as IoError;
-use std::net::SocketAddr;
+use std::net;
 use std::str;
 
 use toml;
 
 pub struct TermcastConfig {
-    pub caster: SocketAddr,
-    pub watcher: SocketAddr,
+    pub caster: net::SocketAddr,
+    pub watcher: net::SocketAddr,
     pub motd: Option<String>,
+}
+
+
+#[derive(Debug)]
+enum ConfigError {
+    Nothing,
+    Invalid(net::AddrParseError),
 }
 
 const CASTER_LISTEN: &'static str = "127.0.0.1:31337";
@@ -44,6 +51,10 @@ fn get_option(toml_value: &toml::Value, option_name: &str) -> Option<String> {
     }
 }
 
+fn parse_socketaddr(addr: String) -> Result<net::SocketAddr, ConfigError> {
+    addr.parse().map_err(ConfigError::Invalid)
+}
+
 impl TermcastConfig {
     pub fn from_config(config_file_path: &str) -> Result<Self, IoError> {
         let mut config = TermcastConfig::default();
@@ -54,13 +65,27 @@ impl TermcastConfig {
         let contents_str = str::from_utf8(&contents).unwrap();
         let mut parser = toml::Parser::new(&contents_str);
         let options = parser.parse().unwrap();
-        println!("{:?}", options);
         if let Some(server_config) = options.get("server") {
-            if let Some(caster_listen) = get_option(&server_config, "caster_listen") {
-                config.caster = caster_listen.parse().unwrap();
+            let c = get_option(&server_config, "caster_listen")
+                        .ok_or(ConfigError::Nothing)
+                        .and_then(parse_socketaddr);
+            match c {
+                Ok(addr) => { config.caster = addr }
+                Err(ConfigError::Invalid(e)) => {
+                    println!("Invalid caster listen address: {}.", e);
+                }
+                Err(_) => { }
             }
-            if let Some(watcher_listen) = get_option(&server_config, "watcher_listen") {
-                config.watcher = watcher_listen.parse().unwrap();
+
+            let c = get_option(&server_config, "watcher_listen")
+                        .ok_or(ConfigError::Nothing)
+                        .and_then(parse_socketaddr);
+            match c {
+                Ok(addr) => { config.watcher = addr }
+                Err(ConfigError::Invalid(e)) => {
+                    println!("Invalid watcher listen address: {}.", e);
+                }
+                Err(_) => { }
             }
         }
 
